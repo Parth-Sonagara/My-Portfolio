648d093a4186de9528fd2bab8e97f5e335dab56b
Parth-Sonagara <parth.sonagara98@gmail.com>
2025-11-19 21:30:06 -0600

    Fix TSX return type issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 
-export default function Home(): JSX.Element {
+export default function Home() {
   const [dark, setDark] = useState<boolean>(false)
 
   const skills: string[] = [
